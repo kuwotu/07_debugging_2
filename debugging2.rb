1a0505573b0d7aa1c5203a3f62affb23d8d91f32
kuwotu <kevwe1996@gmail.com>
2022-08-17 07:45:40 +0100

    debugging most_common_count

--- debugging2.rb
+++ debugging2.rb
@@ -4,16 +4,16 @@ class LetterCounter
     end
   
     def calculate_most_common()
-      counter = Hash.new(1)
+      counter = Hash.new(0)
       most_common = nil
       most_common_count = 1
       @text.chars.each do |char|
         next unless is_letter?(char)
-        counter[char] = (counter[char] || 1) + 1
-        binding.irb
+        counter[char] = (counter[char]) + 1
         if counter[char] > most_common_count
           most_common = char
           most_common_count += counter[char]
+          binding.irb
         end
       end
       return [most_common_count, most_common]
